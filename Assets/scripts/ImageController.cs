196be23a1573bf4b62c4b5393dbcd83281f98df9
junyatakechi <takechi48j@gmail.com>
2020-03-10 20:25:16 +0900

    tried importing images.

---
+++ Assets/scripts/ImageController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageController : MonoBehaviour {
+	public Image image;
+	public Sprite sprite;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Z))
+        {
+			sprite = Resources.Load<Sprite>("shimaji1");
+			image = this.GetComponent<Image>();
+			image.sprite = sprite;
+        }
+	}
+}
